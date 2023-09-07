d191b5a4aeab8ba931a6a3b73c6d8babff4ceb75
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2023-09-07 20:05:41 +0530

    note

--- frontend/src/Board.js
+++ frontend/src/Board.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
 import Navbar from './Components/Navbar';
+import TextArea from './Components/TextArea';
 
 const Board = () => {
   return (
@@ -9,31 +10,14 @@ const Board = () => {
       flexDirection="column"
       minHeight="100vh"
     >
-      <Navbar />
-      <Box
-        flex="1"
-        mx={4}
-        p={4}
-      >
-        <Textarea
-          placeholder="Start typing..."
-          size="lg"
-          resize="none"
-          py={[5, 7, 10]}
-          px={[10, 70, 160]}
-          style={{
-            width: '100%',
-            height: '100%',
-            border: 'none',
-            outline: 'none',
-            fontSize: '16px',
-            lineHeight: '1.5',
-            backgroundColor: 'transparent',
-            boxShadow: 'none',
-            color: 'inherit',
-          }}
-        />
-      </Box>
+        <Navbar />
+        <Box
+            flex="1"
+            mx={4}
+            p={4}
+        >
+            <TextArea/>
+        </Box>
     </Box>
   );
 };
--- frontend/src/Components/TextArea.js
+++ frontend/src/Components/TextArea.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Textarea } from "@chakra-ui/react";
+import { marked } from 'marked';
 
 const TextArea = () => {
-  return (
-    <Textarea
-        placeholder="Start typing..."
-        size="lg"
-        resize="none"
-        py={[5, 7, 10]}
-        px={[10, 70, 160]}
-        style={{
+
+    const [markdownContent, setMarkdownContent] = useState('');
+
+    const handleChange = (event) => {
+        setMarkdownContent(event.target.value);
+    };
+
+    const renderMarkdown = () => {
+        const html = marked(markdownContent);
+        return { __html: html };
+    };
+
+    return (
+        <Textarea
+            placeholder="Start typing..."
+            size="lg"
+            resize="none"
+            py={[5, 7, 10]}
+            px={[10, 70, 160]}
+            style={{
             width: '100%',
             height: '100%',
             border: 'none',
@@ -19,9 +32,12 @@ const TextArea = () => {
             backgroundColor: 'transparent',
             boxShadow: 'none',
             color: 'inherit',
-        }}
-    />
-  )
-}
+            }}
+            value={markdownContent}
+            onChange={handleChange}
+            dangerouslySetInnerHTML={renderMarkdown()}
+        />
+  );
+};
 
-export default TextArea
+export default TextArea;

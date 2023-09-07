814006b5853e55dcb1c4afc3c59b336ca1bf5c0e
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2023-09-07 20:35:14 +0530

    note

--- frontend/src/Components/TextArea.js
+++ frontend/src/Components/TextArea.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-import { Textarea } from "@chakra-ui/react";
+import React, { useState, useRef } from 'react';
 import { marked } from 'marked';
 
 const TextArea = () => {
-
     const [markdownContent, setMarkdownContent] = useState('');
+    const divRef = useRef(null);
 
-    const handleChange = (event) => {
-        setMarkdownContent(event.target.value);
+    const handleChange = () => {
+        if (divRef.current) {
+            setMarkdownContent(divRef.current.innerText);
+        }
     };
 
     const renderMarkdown = () => {
@@ -16,28 +17,71 @@ const TextArea = () => {
     };
 
     return (
-        <Textarea
+        <div
+            ref={divRef}
+            contentEditable
             placeholder="Start typing..."
-            size="lg"
-            resize="none"
-            py={[5, 7, 10]}
-            px={[10, 70, 160]}
             style={{
-            width: '100%',
-            height: '100%',
-            border: 'none',
-            outline: 'none',
-            fontSize: '16px',
-            lineHeight: '1.5',
-            backgroundColor: 'transparent',
-            boxShadow: 'none',
-            color: 'inherit',
+                width: '100%',
+                height: '100%',
+                border: 'none',
+                outline: 'none',
+                fontSize: '16px',
+                lineHeight: '1.5',
+                backgroundColor: 'transparent',
+                boxShadow: 'none',
+                color: 'inherit',
+                whiteSpace: 'nowrap', // Prevent line breaks
             }}
-            value={markdownContent}
-            onChange={handleChange}
+            onInput={handleChange}
             dangerouslySetInnerHTML={renderMarkdown()}
-        />
-  );
+        >{renderMarkdown()}</div>
+    );
 };
 
 export default TextArea;
+
+
+// import React, { useState } from 'react';
+// import { Textarea } from "@chakra-ui/react";
+// import { marked } from 'marked';
+
+// const TextArea = () => {
+
+//     const [markdownContent, setMarkdownContent] = useState('');
+
+//     const handleChange = (event) => {
+//         setMarkdownContent(event.target.value);
+//     };
+
+//     const renderMarkdown = () => {
+//         const html = marked(markdownContent);
+//         return { __html: html };
+//     };
+
+//     return (
+//         <Textarea
+//             placeholder="Start typing..."
+//             size="lg"
+//             resize="none"
+//             py={[5, 7, 10]}
+//             px={[10, 70, 160]}
+//             style={{
+//             width: '100%',
+//             height: '100%',
+//             border: 'none',
+//             outline: 'none',
+//             fontSize: '16px',
+//             lineHeight: '1.5',
+//             backgroundColor: 'transparent',
+//             boxShadow: 'none',
+//             color: 'inherit',
+//             }}
+//             value={markdownContent}
+//             onChange={handleChange}
+//             dangerouslySetInnerHTML={renderMarkdown()}
+//         />
+//   );
+// };
+
+// export default TextArea;
